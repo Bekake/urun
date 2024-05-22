84e3122bc9da5fc29d0f5417973b64a515e6c07e
Bekake <167704943+Bekake@users.noreply.github.com>
2024-05-23 02:07:45 +0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Bekake/bir.git main:gh-pages
+git push -f git@github.com:Bekake/urun.git main:gh-pages
 
 cd -
